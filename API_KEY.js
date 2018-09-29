2d28c2f4cb7f3eef533177c83beaa0b002f285ce
Padmanabh Gupta <padmanabh14@gmail.com>
2018-09-29 19:30:41 +0530

    Added appliance webhook

---
+++ API_KEY.js
@@ -0,0 +1,3 @@
+const apiKey = 'e44c4bdf-a737-4499-96f8-b5ccecc1f393'
+
+module.exports = apiKey;
--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const http = require('http');
+const HEROKU_API_KEY = require('./API_KEY');
+
+
+
+const server = express();
+
+server.use('body-parser');
+
+
+server.post('/appliance',(request,response) => {
+    console.log('---------------');
+    console.log(request.body.params);
+    console.log('---------------');
+})
